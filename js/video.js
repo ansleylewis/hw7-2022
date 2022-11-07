c4a38f785b41bc06e7fbb601ba701a28edb25036
ansleylewis <112427884+ansleylewis@users.noreply.github.com>
2022-11-07 10:41:20 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,78 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector('#player1')
+	console.log(video)
+	video.autoplay = false;
+	video.loop = false; 
+
+	console.log("Auto play is set to " + video.autoplay);
+	console.log("Loop is set to " + video.loop);
+});
+
+ document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	document.querySelector("#volume").innerHTML = video.volume * 100 + '%'
+	video.play()
+ });
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	document.querySelector("#volume").innerHTML = video.volume * 100 + '%'
+	video.pause(video)
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate *= 0.9
+	console.log("The new speed is " + (video.playbackRate * 100)) 
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up");
+	video.playbackRate *= 1.10
+	console.log("The new speed is " + (video.playbackRate * 100)) 
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead");
+	video.currentTime += 10 
+
+	if (video.currentTime >= 67.4033){
+		video.currentTime= 0
+	}
+
+	console.log("The new video time is " + video.currentTime) 
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {   
+		video.muted = true;
+		console.log("Video Muted")
+		document.querySelector("#mute").innerHTML = "Unmute";
+	 	}
+ 
+	else {
+		video.muted = false;
+		console.log("Video Unmuted")
+		document.querySelector("#mute").innerHTML = "Mute";
+	 	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log("The current volume is " + video.volume);
+	video.volume = this.value / 100
+	console.log("The current volume is " + video.volume);
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + '%';
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Old School On")
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Original On")
+	video.classList.remove("oldSchool");
+});
